fb1ebe0554b034593231cf6d5fefa3c133194115
guideveloperment <viana.guilherme29@gmail.com>
2021-08-20 23:06:29 -0300

    API

---
+++ forum/src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/")
+
+	public String hello() {
+		return "Hello Word!";
+	}
+
+}
---
+++ forum/src/main/java/br/com/alura/forum/controller/repository/CursoRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.controller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso, Long>  {
+
+	static Curso findByNome(String nome) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
